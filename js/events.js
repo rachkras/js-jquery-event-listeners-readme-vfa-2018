b9bb2cb63e54187e1438c87218c37848f42eadbb
Rachel Krasnick <rachkras@umich.edu>
2018-06-28 15:57:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ $("form").on('keydown', function(key) {
 
 function submitIt(){
   $("form").on("submit", function() {
-    alert('your form is going to be submitted now');
+    alert("Your form is going to be submitted now");
 });
 }
 
